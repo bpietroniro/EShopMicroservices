a15caf9dd3b45b8479755f859186fc22f22553d4
Bethany Pietroniro <bethany.pietroniro@wolfjawstudios.com>
2025-02-21 14:04:00 -0500

    define ordering domain events

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -3,7 +3,7 @@
     public record OrderName
     {
         private const int DefaultLength = 5;
-        public string Value { get; };
+        public string Value { get; }
         private OrderName(string value) => Value = value;
         public static OrderName Of(string value)
         {
