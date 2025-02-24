6dcbdea5d9f2dc5f22124ed950ab3fed16c6a5d1
Bethany Pietroniro <bethany.pietroniro@wolfjawstudios.com>
2025-02-24 13:06:05 -0500

    add get orders query with pagination

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BuildingBlocks.Pagination;
 
 namespace Ordering.Application.Orders.Queries.GetOrders
 {
-    internal class GetOrdersHandler
+    public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
     {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            var pageIndex = query.PaginationRequest.PageIndex;
+            var pageSize = query.PaginationRequest.PageSize;
+
+            var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+            var orders = await dbContext.Orders
+                           .Include(o => o.OrderItems)
+                           .OrderBy(o => o.OrderName.Value)
+                           .Skip(pageSize * pageIndex)
+                           .Take(pageSize)
+                           .ToListAsync(cancellationToken);
+
+            return new GetOrdersResult(
+                new PaginatedResult<OrderDto>(
+                    pageIndex,
+                    pageSize,
+                    totalCount,
+                    orders.ToOrderDtoList()));
+        }
     }
 }
