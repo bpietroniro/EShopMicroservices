70cb96a2159f02c589a1dacb9cb3ff6972296a75
Bethany Pietroniro <bethany.pietroniro@wolfjawstudios.com>
2025-02-24 14:55:40 -0500

    add API endpoint implementations

---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.DataTransferObjects;
+global using Ordering.Application.Orders.Commands.CreateOrder;
+
