e9e9e04d98d0e3e806d2cff546774ea857c93288
Bethany Pietroniro <bethany.pietroniro@wolfjawstudios.com>
2025-02-24 12:25:22 -0500

    fill out basic CRUD logic for ordering application layer

--- src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace Ordering.Application.Data
         DbSet<Product> Products { get; }
         DbSet<Order> Orders { get; }
         DbSet<OrderItem> OrderItems { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.DataTransferObjects;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Application.Exceptions;
+
