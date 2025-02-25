c194a70e206d2b10eda1c97918093c60a43393e7
Bethany Pietroniro <bethany.pietroniro@wolfjawstudios.com>
2025-02-25 11:49:04 -0500

    connect basket and ordering services via message queue; dockerize all services

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -7,7 +7,11 @@ namespace BuildingBlocks.Messaging.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+        public static IServiceCollection AddMessageBroker(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            Assembly? assembly = null
+        )
         {
             // implement RabbitMQ MassTransit configuration
             services.AddMassTransit(config =>
@@ -19,15 +23,20 @@ namespace BuildingBlocks.Messaging.MassTransit
                     config.AddConsumers(assembly);
                 }
 
-                config.UsingRabbitMq((context, configurator) =>
-                {
-                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                config.UsingRabbitMq(
+                    (context, configurator) =>
                     {
-                        host.Username(configuration["MessageBroker:UserName"]!);
-                        host.Password(configuration["MessageBroker:Password"]!);
-                    });
-                    configurator.ConfigureEndpoints(context);
-                });
+                        configurator.Host(
+                            new Uri(configuration["MessageBroker:Host"]!),
+                            host =>
+                            {
+                                host.Username(configuration["MessageBroker:UserName"]!);
+                                host.Password(configuration["MessageBroker:Password"]!);
+                            }
+                        );
+                        configurator.ConfigureEndpoints(context);
+                    }
+                );
             });
             return services;
         }
